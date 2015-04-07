cb1569d47a4aecc007341b908e78e96b0d17cfd3
Jeremiah Lant <jlant@usgs.gov>
2015-04-06 22:16:07 -0400

    completed first homework for lesson 2 about rot13 text encoding

--- lesson-02/homework/homework.py
+++ lesson-02/homework/homework.py
@@ -5,15 +5,25 @@ form = """
 <form method="post">
     <h1>Enter some text to ROT13</h1>
     <br>
-    <textarea rows="6" cols="50">Hello there!</textarea>
+    <textarea name="text" rows="6" cols="50">{text}</textarea>
     <br>
+    <p>{error}</p>
     <input type="submit">
 </form>
 """
 
 class MainPage(webapp2.RequestHandler):
+    
+    def write_form(self, error = "", text = "Hello there!"):
+        self.response.out.write(form.format(error = error, text = text))
+
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
-        self.response.write(form)
+        self.write_form()
+
+    def post(self):
+        text = self.request.get("text")
+        rot13_text = text.encode("rot13")
+        self.write_form(text = rot13_text)
 
 application = webapp2.WSGIApplication([('/', MainPage),], debug=True)
